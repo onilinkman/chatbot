2f121e65dafce5ac723c61e63da29b8dae1f6da3
cmarban <cmarban@umsa.bo>
2025-08-05 11:17:42 -0400

    chatbot original

--- src/bot/bot.controller.ts
+++ src/bot/bot.controller.ts
@@ -5,30 +5,39 @@ import PostMensajeDto from './dto/post-mensaje.dto';
 
 @Controller('api/bot')
 export class BotController {
-  constructor(private readonly botService: BotService) {}
+    constructor(private readonly botService: BotService) {}
 
-  /* @Get()
+    /* @Get()
   getAllBots() {
     return this.botService.enviarMensaje();
   } */
 
-  @Post('/enviar')
-  async postMensaje(@Body() postMensajeDto: PostMensajeDto) {
-	//Nota: enviar con este formato: 59179161442@s.whatsapp.net
-    try {
-      await this.botService.enviarMensaje(
-        postMensajeDto.nro_telefono,
-        postMensajeDto.mensaje,
-      );
-      return { body: 'Mensaje enviado' };
-    } catch (error) {}
-  }
+    @Post('/enviar')
+    async postMensaje(@Body() postMensajeDto: PostMensajeDto) {
+        //Nota: enviar con este formato: 59179161442@s.whatsapp.net
+        try {
+            await this.botService.enviarMensaje(
+                postMensajeDto.nro_telefono,
+                postMensajeDto.mensaje,
+            );
+            return { body: 'Mensaje enviado' };
+        } catch (error) {}
+    }
 
-  @Get('/conectar')
-  conectarWhatsapp(@Req() request: Request, @Res() response: Response) {
-    this.botService.conectarWhatsapp();
-    return response.status(200).send({
-      mensaje: 'hola',
-    });
-  }
+    @Get('/conectar')
+    conectarWhatsapp(@Req() request: Request, @Res() response: Response) {
+        let sent = false;
+        this.botService.conectarWhatsapp((qrUrl) => {
+            if (sent) return;
+            sent = true;
+            const base64Data = qrUrl.replace(/^data:image\/png;base64,/, '');
+            const imgBuffer = Buffer.from(base64Data, 'base64');
+            response.setHeader('Content-Type', 'image/png');
+            response.status(200).send(imgBuffer);
+        });
+        /* this.botService.conectarWhatsapp();
+        return response.status(200).send({
+            mensaje: 'hola',
+        }); */
+    }
 }
--- src/bot/bot.service.ts
+++ src/bot/bot.service.ts
@@ -1,29 +1,25 @@
 import { Injectable } from '@nestjs/common';
 import makeWASocket, {
-    AuthenticationState,
     DisconnectReason,
-    initAuthCreds,
     proto,
-    SignalDataTypeMap,
     useMultiFileAuthState,
     WASocket,
 } from 'baileys';
 import P from 'pino';
-import { toString } from 'qrcode';
-import { CreateSesionWhatsappDto } from 'src/sesion_whatsapp/dto/create-sesion_whatsapp.dto';
-import { UpdateSesionWhatsappDto } from 'src/sesion_whatsapp/dto/update-sesion_whatsapp.dto';
-import { SesionWhatsappService } from 'src/sesion_whatsapp/sesion_whatsapp.service';
+import { toDataURL, toString } from 'qrcode';
 
 @Injectable()
 export class BotService {
     private sock: WASocket | null = null;
+    private mapSock: Map<String, WASocket> | null = null;
 
-    constructor(
-        private readonly sesionWhatsappService: SesionWhatsappService,
-    ) {}
+    constructor() {}
 
-    async conectarWhatsapp() {
-        const { state, saveCreds } = await this.useDatabaseAuthState(1);
+    async conectarWhatsapp(callback: (qrUrl: string) => void) {
+        /* const { state, saveCreds } =
+            await useMultiFileAuthState('auth_info_baileys'); */
+        const { state, saveCreds } = await useMultiFileAuthState('client-one');
+        //const { state, saveCreds } = await this.useDatabaseAuthState(1);
 
         this.sock = makeWASocket({
             auth: state,
@@ -35,11 +31,14 @@ export class BotService {
         this.sock.ev.on('connection.update', async (update) => {
             const { qr } = update;
             if (typeof qr === 'string' && qr.length > 0) {
-                const qrTerminal = await toString(qr, {
+                /* const qrTerminal = await toString(qr, {
                     type: 'terminal',
                     small: true,
                 });
-                console.log(qrTerminal);
+                console.log(qrTerminal); */
+
+                const qrUrl = await toDataURL(qr);
+                callback(qrUrl);
             }
             const { connection, lastDisconnect } = update;
             if (connection === 'close') {
@@ -47,8 +46,7 @@ export class BotService {
                     (lastDisconnect?.error as any)?.output?.statusCode !==
                     DisconnectReason.restartRequired;
                 if (puedeConectar) {
-                    console.log('conectando....');
-                    this.conectarWhatsapp();
+                    this.conectarWhatsapp((qrr) => {});
                 }
             } else if (connection == 'open') {
                 console.log('CONEXION ABIERTA');
@@ -93,95 +91,5 @@ export class BotService {
             text: mensaje,
         });
         return { mensaje: 'mensaje enviado' };
-    }
-
-    async useDatabaseAuthState(id: number): Promise<{
-        state: AuthenticationState;
-        saveCreds: () => Promise<void>;
-    }> {
-        // 1. Leer credenciales y llaves desde la base de datos
-        const sesion_whatsapp =
-            await this.sesionWhatsappService.findOneByName('client-one');
-        const credsFromDb = sesion_whatsapp?.creds; //tuDb.obtener('wa_creds');
-        const keysFromDb = sesion_whatsapp?.keys; //tuDb.obtener('wa_keys');
-
-        let creds: AuthenticationState['creds'];
-        if (credsFromDb) {
-            creds =
-                typeof credsFromDb === 'string'
-                    ? JSON.parse(credsFromDb)
-                    : credsFromDb;
-        } else {
-            creds = initAuthCreds();
-        }
-        let keys: Partial<SignalDataTypeMap> = {};
-        if (keysFromDb) {
-            keys =
-                typeof keysFromDb === 'string'
-                    ? JSON.parse(keysFromDb)
-                    : keysFromDb;
-        }
-
-        const state: AuthenticationState = {
-            creds,
-            keys: {
-                get: async (type, ids) => {
-                    // Recupera las llaves del objeto keys en memoria
-                    const result: any = {};
-                    for (const id of ids) {
-                        if (keys[type] && keys[type][id]) {
-                            result[id] = keys[type][id];
-                        }
-                    }
-                    return result;
-                },
-                set: async (data) => {
-                    // Actualiza el objeto keys en memoria
-                    for (const _type in data) {
-                        if (!keys[_type]) keys[_type] = {};
-                        Object.assign(keys[_type], data[_type]);
-                    }
-                },
-            },
-        };
-
-        const saveCreds = async () => {
-            if (!state.creds) {
-                console.error('No hay creds para guardar');
-                return;
-            }
-            if (!state.keys) {
-                console.error('No hay keys para guardar');
-                return;
-            }
-
-            const createSesionWhatsappDto: CreateSesionWhatsappDto = {
-                creds: JSON.stringify(state.creds),
-                keys: JSON.stringify(keys), // <-- agrega esto 
-                name: 'client-one',
-                telefono: '123456789',
-            };
-            try {
-                const sw =
-                    await this.sesionWhatsappService.findOneByName(
-                        'client-one',
-                    );
-                if (sw) {
-                    await this.sesionWhatsappService.update(1, {
-                        creds: JSON.stringify(creds),
-                        keys: JSON.stringify(keys),
-                    });
-                } else {
-                    await this.sesionWhatsappService.create(
-                        createSesionWhatsappDto,
-                    );
-                }
-            } catch (error) {
-                console.error('Error guardando sesión:', error);
-            }
-            // Guarda también las llaves si es necesario
-        };
-
-        return { state, saveCreds };
     }
 }
